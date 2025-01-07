942b01cf80b9204e65d429b5e933fc598e0249e3
devopstest25 <devopscsbs@gmail.com>
2025-01-07 09:51:40 +0530

    Update a.py

--- a.py
+++ a.py
@@ -15,3 +15,6 @@ print(f"Subtraction: {num1} - {num2} = {subtraction}")
 print(f"Multiplication: {num1} * {num2} = {multiplication}")
 print(f"Division: {num1} / {num2} = {division}")
 print("this is a calculator")
+
+
+# written by Priyanshu Kumar Sinha
